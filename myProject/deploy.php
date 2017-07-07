2e4f65d1cf4d052b0d274760516b5007b5690fae
Yasunori Takahashi <hibriiiiidge@gmail.com>
2017-07-07 10:50:47 +0900

    deployの修正

--- myProject/deploy.php
+++ myProject/deploy.php
@@ -31,13 +31,14 @@ after('deploy:symlink', 'php-fpm:restart');
 // [Optional] if deploy fails automatically unlock.
 after('deploy:failed', 'deploy:unlock');
 
-task('deploy:migrate', function(){
-  run('{{bin/php}} /var/www/html/releases/1/myProject/artisan migrate --force');
-});
-
-// task('deploy:migrate', function () {
-// run('{{bin/php}} {{release_path}}/artisan migrate --force');
+// task('deploy:migrate', function(){
+//   run('{{bin/php}} /var/www/html/releases/1/myProject/artisan migrate --force');
 // });
+
+task('deploy:migrate', function () {
+// run('{{bin/php}} {{release_path}}/artisan migrate --force');
+run('var_dump({{release_path}})');
+});
 
 
 // Migrate database before symlink new release.
@@ -58,55 +59,3 @@ task('deploy', [
     'cleanup',
     'success'
 ]);
-
-
-// require 'recipe/laravel.php';
-//
-// // デフォルトがUTCなので上書き
-// env('timezone', 'Asia/Tokyo');
-//
-// // デプロイ先のサーバ情報を定義
-// //serverList('config/server.yml');
-// server('myProject', '13.114.47.97', 22)
-//     ->user('ubuntu')
-//     // ssh agentを使い認証します。
-//     ->forwardAgent()
-//     ->stage('production')
-//     // デプロイ先のベースパスを定義します。
-//     ->env('deploy_path', '/var/www/html/');
-//
-// // デプロイしたプロジェクトを直近5件保存しておく (デフォルトは3件)
-// // -> 保存しておいた分だけrollbackコマンドを使用できる
-// set('keep_releases', 5);
-//
-// // デプロイするリポジトリのURL
-// set('repository', 'git@github.com:hibriiiiidge/LaravelProject.git');
-//
-// // shared_dirsを上書き (deploy:sharedで使用)
-// // -> 対象ディレクトリがなければ作ってくれる
-// set('shared_dirs', [
-//     'storage/app',
-//     'storage/framework/cache',
-//     'storage/framework/sessions',
-//     'storage/framework/views',
-//     'storage/logs',
-// ]);
-//
-// // shared_filesを上書き (deploy:sharedで使用)
-// // -> 対象ファイルがなければ空ファイルを作ってくれる
-// // -> sqliteを使う場合、ここで設定しておくと良さそう(?)
-// set('shared_files', [
-//     '.env',
-//     // 'database/database.sqlite',
-// ]);
-//
-// // writable_dirsを上書き (deploy:writableで使用)
-// // -> デプロイ先の環境によってパーミッションの変更方法が変わる
-// set('writable_dirs', [
-//     'bootstrap/cache',
-//     'storage/app',
-//     'storage/framework/cache',
-//     'storage/framework/sessions',
-//     'storage/framework/views',
-//     'storage/logs',
-// ]);
